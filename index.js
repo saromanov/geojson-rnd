2723e1a146c30ce60fa3dc669c76ba67767c0115
Sergey <xxsmotur@gmail.com>
2020-02-28 16:49:25 +0500

    Add draft for line generating

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { random } from "math";
+import { random, PI } from "math";
 
 const point = coords => {
     return {
@@ -15,6 +15,19 @@ const polygon = coords => {
 }
 
 const Point = (count) => {
-    var gen = () => {random() * 100}
-    return {p1: gen(), p2: gen()};
+    var gen = () => {random() - 0.5}
+    return {p1: gen()*360, p2: gen()*180};
+}
+
+const Line = (length) => {
+    let angle = random()*2 * PI;
+    let data_angle = angle + (random() - 0.5) * 2;
+}
+
+const gen_line = () => {
+    points = [];
+    for(i = 0;i < 3;i++) {
+        points.push(Point(2));
+    }
+    return points;
 }
